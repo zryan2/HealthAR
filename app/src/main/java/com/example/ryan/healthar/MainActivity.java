d7e04b3ae49683826612a0d9fc83b2a525931cfa
Ryan Zeng <zeng.ryan2@gmail.com>
2018-02-24 04:17:05 -0500

    Added AR capability

--- app/src/main/java/com/example/ryan/healthar/MainActivity.java
+++ app/src/main/java/com/example/ryan/healthar/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.ryan.healthar;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.ArCoreApk.*;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+
 // Test test
 public class MainActivity extends AppCompatActivity {
+    Button mArButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mArButton = (Button) findViewById(R.id.mArButton);
+        maybeEnableArButton();
+    }
+
+    void maybeEnableArButton() {
+        // Likely called from Activity.onCreate() of an activity with AR buttons.
+        Availability availability = ArCoreApk.getInstance().checkAvailability(this);
+        if (availability.isTransient()) {
+            // re-query at 5Hz while we check compatibility.
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    maybeEnableArButton();
+                }
+            }, 200);
+        }
+        if (ArCoreApk.getInstance().checkAvailability(this).isSupported()) {
+            mArButton.setVisibility(INVISIBLE);
+            mArButton.setEnabled(true);
+            // indicator on the button.
+        } else { // unsupported or unknown
+            mArButton.setVisibility(VISIBLE);
+            mArButton.setEnabled(false);
+        }
     }
 }

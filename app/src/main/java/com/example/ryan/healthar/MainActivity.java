715b16b191311709d7b8a09e3900a2122a9be465
Ryan Zeng <zeng.ryan2@gmail.com>
2018-02-24 03:09:07 -0500

    Initial Commit

--- app/src/main/java/com/example/ryan/healthar/MainActivity.java
+++ app/src/main/java/com/example/ryan/healthar/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.ryan.healthar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-// Test
+// Test test
 public class MainActivity extends AppCompatActivity {
 
     @Override

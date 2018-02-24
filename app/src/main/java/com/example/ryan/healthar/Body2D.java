b4e0cbbf508385e7eafccb31f60bac680c327f9e
Ryan Zeng <zeng.ryan2@gmail.com>
2018-02-24 06:35:55 -0500

    Added AR and nonAR activties Started OpenGL capabilities

---
+++ app/src/main/java/com/example/ryan/healthar/Body2D.java
@@ -0,0 +1,46 @@
+package com.example.ryan.healthar;
+
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Body2D extends Fragment {
+    private OpenGLView body2D;
+
+    public Body2D() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_2d_body, container, false);
+        body2D = (OpenGLView) v.findViewById(R.id.body2D);
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        body2D.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        body2D.onPause();
+    }
+
+}
